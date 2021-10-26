b858c7f0d8ddc8ebdfb0e6c2b37c5300907b8e22
Lucas <lukasz.bielenin@microsoft.wsei.edu.pl>
2021-10-26 22:14:02 +0200

    Fix references

---
+++ tests/LoanManagerTests.cs
@@ -0,0 +1,70 @@
+using Loans_application;
+using Loans_application.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Loan_application.Tests
+{
+    public class LoanManagerTests
+    {
+        [Fact]
+        public void LoanManager_ManageLoans_DistributesLoansValidly()
+        {
+            Dictionary<int, int> expectedResultByIds = new() { { 1, 0 }, { 2, 1 }, { 3, 2 }, { 4, 1 } };
+            var manager = new LoanManager(TestData.GetLoans(), TestData.GetFacilities(), TestData.GetCovenants());
+
+            var result = manager.ManageLoans().OrderBy(l => l.Loan.Id).ToList();
+
+            result.ForEach(fac => 
+                Assert.Equal(expectedResultByIds[fac.Loan.Id], fac.Loan.FacilityId)
+                );
+        }
+
+        [Fact]
+        public void LoanManager_ManageLoans_WithTooMuchLoans_StillShoudPerformCaluctionForRest ()
+        {
+            Dictionary<int, int> expectedResultByIds = new() { { 1, 0 }, { 2, 1 }, { 3, 2 }, { 4, 1 } };
+            var loans = TestData.GetLoans();
+            loans.Add(new() { Amount = 1234, FacilityId = 2, InterestRate = 0.00, DefaultLikelihood = 0.1, Id = 23 });
+
+            var manager = new LoanManager(loans, TestData.GetFacilities(), TestData.GetCovenants());
+
+            var result = manager.ManageLoans().OrderBy(l => l.Loan.Id).ToList();
+
+            result.ForEach(fac =>
+                Assert.Equal(expectedResultByIds[fac.Loan.Id], fac.Loan.FacilityId)
+                );
+        }
+
+
+        [Fact]
+        public void LoanManager_ManageLoans_WithInvalidLoans_ShouldReturnEmptyList()
+        {
+            var loans = 
+                new List<Loan> { new () { Amount = 1000000, DefaultLikelihood = 0.0, Id = 1, InterestRate = 0.0, State = "CAL" } };
+
+            var manager = new LoanManager(loans, TestData.GetFacilities(), TestData.GetCovenants());
+
+            var result = manager.ManageLoans().OrderBy(l => l.Loan.Id).ToList();
+
+            Assert.Empty(result);
+        }
+
+        [Theory]
+        [InlineData(200, 0.2, 0.3, 312)]
+        [InlineData(500, 0.15, 0.22, 701.5)]
+        public void LoanFacilityPair_CalculatesCostsValidly(
+            double amount, double loanInterestRate, double facilityInterestRate, double expectedResult)
+        {
+            LoanFacilityPair testPair =
+                new
+                (
+                    new Loan() { Amount = amount, InterestRate = loanInterestRate },
+                    new Facility { InterestRate = facilityInterestRate }
+                );
+
+            Assert.Equal(expectedResult, testPair.Cost);
+        }
+    }
+}
---
+++ tests/TestData.cs
@@ -0,0 +1,31 @@
+﻿using Loans_application.Models;
+using System.Collections.Generic;
+
+namespace Loan_application.Tests
+{
+    public static class TestData
+    {
+        public static List<Loan> GetLoans() =>
+            new()
+            {
+                new() { Amount = 100, DefaultLikelihood = 0.2, Id = 1, State = "CA", InterestRate = 0.15 },
+                new() { Amount = 150, DefaultLikelihood = 0.2, Id = 2, State = "MAS", InterestRate = 0.20 },
+                new() { Amount = 200, DefaultLikelihood = 0.4, Id = 3, State = "TX", InterestRate = 0.15 },
+                new() { Amount = 100, DefaultLikelihood = 0.2, Id = 4, State = "NY", InterestRate = 0.20 },
+            };
+        public static List<Facility> GetFacilities() =>
+            new()
+            {
+                new() { Id = 0, Amount = 100, BankId = 666, InterestRate = 0.2 },
+                new() { Id = 1, Amount = 1000, BankId = 666, InterestRate = 0.2 },
+                new() { Id = 2, Amount = 200, BankId = 666, InterestRate = 0.2 },
+            };
+
+        public static List<Covenant> GetCovenants() =>
+            new()
+            {
+                new() { BankId = 1, BannedState = "TX", MaxDefaultLikelihood = 03, FacilityId = 1 }
+            };
+
+    }
+}
